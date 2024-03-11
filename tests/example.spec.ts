d6a4395ae97f0937d5c52b0dc71e545144b37bd3
priya-acko <101645382+priya-acko@users.noreply.github.com>
2024-03-11 18:43:41 +0530

    Update example.spec.ts
    Removed only

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -124,7 +124,7 @@ let mobileNumber = mobile.mobileNumber();
         await playwrightDev .Payment('Platnium');
            
      });
-   test.only('E2E testing of porting jounrney yearly ', async ({ page }) => {
+   test('E2E testing of porting jounrney yearly ', async ({ page }) => {
             test.slow()
             const mobile = new BasePage()
             let mobileNumber =  mobile.mobileNumber();
