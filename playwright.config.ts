c64310dc48ad7b69c5f79b077f330ec01390f389
priya-acko <101645382+priya-acko@users.noreply.github.com>
2024-03-28 00:06:57 +0530

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,8 +35,8 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
      baseURL: 'https://www.ackodev.com/',
-    //  screenshot: 'on',
-    //  video: 'on',
+      screenshot: 'on',
+      video: 'on',
 
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
