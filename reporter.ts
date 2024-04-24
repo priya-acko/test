e0718897cd10e8f9723cf977c6dcf7351e489505
kollajhansi.lakshmi <kollajhansi.lakshmi@acko.tech>
2024-04-24 23:52:00 +0530

    added the code for dashboard integration

--- reporter.ts
+++ reporter.ts
@@ -1,10 +1,59 @@
+import { TestInfo } from '@playwright/test';
 import type {
     FullConfig, FullResult, Reporter, Suite, TestCase, TestResult
   } from '@playwright/test/reporter';
+  import axios from 'axios';
   
   class MyReporter implements Reporter {
-    onBegin(config: FullConfig, suite: Suite) {
+
+    teamId: string | null;
+    totalCasesPassed: number;
+    totalCasesSkipped: number;
+    totalCasesFailed: number;
+    testsCompleted: number;
+
+
+    constructor() {
+      this.teamId = null;
+      this.totalCasesPassed = 0;
+      this.totalCasesSkipped = 0;
+      this.totalCasesFailed = 0;
+      this.testsCompleted = 0;
+  }
+   async onBegin(config: FullConfig, suite: Suite) {
       console.log(`Starting the run with ${suite.allTests().length} tests`);
+      console.log("current running file name = " + suite.suites[0].suites[0].title.split(".")[0].split("/").slice(1));
+      const now = new Date();
+        const startTime = now.toISOString();
+        const serviceName = suite.suites[0].suites[0].title.split(".")[0].split("/").slice(1)
+        const teamName = "health";
+        const category = "Regression";
+        console.log('Before all tests' + serviceName);
+        try {
+            const requestBody = {
+                "teamName": "health",
+                "serviceName": serviceName.toString().toLowerCase(),
+                "category": "REGRESSION",
+                "startTime": startTime
+            };
+            console.log("request  body of the api = " + JSON.stringify(requestBody));
+
+            const response = await axios.post("http://localhost:1517/testrun/generateid", requestBody);
+
+            console.log("------------------");
+            console.log(response.data);
+            this.teamId = response.data.runId;
+            console.log("-------------------");
+        } catch (error) {
+            if (error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+            } else if (error.request) {
+                console.log(error.request);
+            } else {
+                console.log("Error", error.message);
+            }
+        }
     }
   
     onTestBegin(test: TestCase, result: TestResult) {
@@ -13,10 +62,54 @@ import type {
   
     onTestEnd(test: TestCase, result: TestResult) {
       console.log(`Finished test ${test.title}: ${result.status}`);
+      if (result.status == "passed") {
+        this.totalCasesPassed++;
+    }
+    if (result.status == "failed" || result.status == "timedOut") {
+        this.totalCasesFailed++;
+    }
+    if (result.status == "skipped") {
+        this.totalCasesSkipped++;
+    }
     }
   
-    onEnd(result: FullResult) {
+   async onEnd(result: FullResult) {
       console.log(`Finished the run: ${result.status}`);
+
+      console.log('After all tests');
+      let testSummary = {
+          "testCasesPassed": this.totalCasesPassed,
+          "testCasesFailed": this.totalCasesFailed,
+          "testCasesSkipped": this.totalCasesSkipped
+      }
+      const now = new Date();
+      const currentTimeUTC = now.toISOString();
+      let endTime = currentTimeUTC;
+      try {
+          const requestBody = {
+              "testSummary": testSummary,
+              "endTime": endTime,
+              "status": "completed",
+              "report" : "http://jenkins-qa.ackodev.com/job/playwright_retail/ws/playwright-report/index.html/"
+          };
+          console.log("request body = " + JSON.stringify(requestBody));
+
+          const response = await axios.post("http://localhost:1517/testrun/" + this.teamId + "/update", requestBody);
+
+          console.log("------------------");
+          console.log(response.data);
+          console.log("-------------------");
+      } catch (error) {
+          if (error.response) {
+              console.log(error.response.data);
+              console.log(error.response.status);
+          } else if (error.request) {
+              console.log(error.request);
+          } else {
+              console.log("Error", error.message);
+          }
+      }
+
     }
   }
   
