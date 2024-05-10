87941f7987c248bee7830c03de5a3607e855a599
priya-acko <101645382+priya-acko@users.noreply.github.com>
2024-05-10 15:27:29 +0530

    Removed only

--- tests/spec/buyHealth.spec.ts
+++ tests/spec/buyHealth.spec.ts
@@ -125,7 +125,7 @@ let mobileNumber = mobile.mobileNumber();
         await playwrightDev .Payment('Platnium');
            
      });
-   test.only('E2E testing of porting journey yearly ', async ({ page }) => {
+   test('E2E testing of porting journey yearly ', async ({ page }) => {
         test.setTimeout(120000)
             const mobile = new BasePage()
             let mobileNumber =  mobile.mobileNumber();
